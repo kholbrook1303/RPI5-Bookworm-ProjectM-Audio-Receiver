ab57cab50bbf9cfdefbb058f4df24bb476d898e3
kholbrook1303 <kholbrook1303@gmail.com>
2024-04-03 09:42:05 -0500

    code cleanup and organization

---
+++ lib/config.py
@@ -0,0 +1,63 @@
+import os
+import sys
+
+from configparser import ConfigParser, RawConfigParser
+
+APP_ROOT = os.path.join(
+    os.path.dirname(
+        os.path.abspath(__file__)
+        ), '..'
+    )   
+
+class Config:
+    def __init__(self, config_path, config_header=None):
+        try:
+            if config_header:
+                config = RawConfigParser(allow_no_value=True)
+                with open(config_path) as config_file:
+                    data = config_file.read()
+                    config.read_string(config_header + '\n' + data)
+                    
+            else:
+                config = ConfigParser(allow_no_value=True)
+                config.read(config_path)
+
+            for section in config.sections():
+                setattr(self, section, dict())
+
+                for name, str_value in config.items(section):
+                    if name.endswith("_devices"):
+                        value = config.get(section, name).split(",")
+                    elif self._is_str_bool(str_value):
+                        value = config.getboolean(section, name)
+                    elif self._is_str_int(str_value):
+                        value = config.getint(section, name)
+                    else:
+                        value = config.get(section, name)
+
+                    getattr(self, section)[name] = value
+
+        except Exception as e:
+            print ("Error loading configuration file", e)
+            sys.exit(-1)
+        
+    def _is_str_bool(self, value):
+        """Check if string is an integer.
+        @param value: object to be verified.
+        """
+        if value.lower() == 'true':
+            return True
+        elif value.lower() == 'false':
+            return True
+
+        return False
+
+    def _is_str_int(self, value):
+        """Check if string is an integer.
+        @param value: object to be verified.
+        """
+        try:
+            int(value)
+            return True
+        except:
+            return False
